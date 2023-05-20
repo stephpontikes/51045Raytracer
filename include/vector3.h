36ca15fd2bce3f4ee489a3ef6a03f2c8302d3663
Logan Sherwin <lsherwin10@gmail.com>
2023-05-20 00:01:08 -0500

    Add *, +, - operators to vectors

--- include/vector3.h
+++ include/vector3.h
@@ -29,6 +29,48 @@ struct Vector3 {
         z /= dist;
     }
 
+    static Vector3<T> cross(const Vector3<T>& u, const Vector3<T>& v) {
+        Vector3<T> res{(u.y * v.z) - (u.z * v.y),
+                       -((u.x * v.z) - (u.z * v.x)),
+                       (u.x * v.y) - (u.y * v.x)};
+
+        return res;
+    }
+
+    Vector3<T>& operator-=(const Vector3<T>& rhs) {
+        this->x = this->x - rhs.x;  // This is the easy one
+        this->y = this->y - rhs.y;
+        this->z = this->z - rhs.z;
+
+        return *this;
+    }
+
+    friend Vector3<T> operator-(Vector3<T> lhs, const Vector3<T>& rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+
+    Vector3<T>& operator+=(const Vector3<T>& rhs) {
+        this->x = this->x + rhs.x;  // This is the easy one
+        this->y = this->y + rhs.y;
+        this->z = this->z + rhs.z;
+
+        return *this;
+    }
+
+    friend Vector3<T> operator+(Vector3<T> lhs, const Vector3<T>& rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+
+    friend Vector3<T> operator*(Numeric auto scalar, const Vector3<T>& rhs) {
+        return Vector3<T>{rhs.x * scalar, rhs.y * scalar, rhs.z * scalar};
+    }
+
+    friend Vector3<T> operator*(const Vector3<T>& lhs, Numeric auto scalar) {
+        return scalar * lhs;
+    }
+
     T x = 0;
     T y = 0;
     T z = 0;
